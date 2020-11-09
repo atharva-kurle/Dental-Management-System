5168f8230b76c40e73062e0089bcedb4fbefb49b
contfrelnc <contfrelnc@gmail.com>
2020-11-09 19:09:03 +0530

    user front-end 50% done

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="container">
+    <h1 class="banner cyan-text">YOUR OWN <br> STOMATOLOGIST</h1>
+    <p>Lorem ipsum Nobis delectus rem nisi quo praesentium, so<br>luta ipsa porro eaque, dicta harum saepe rerum dolorem volu<br>orem ipsum Nobis delectus rem nisi quo praesentium, so<br>ptates eveniet iure modi, quibusdam maxime cum.</p>
+</div>
+<div class="container">
+    <form action="" method="get">
+        <button class="btn-large white-text black login_btn">LOGIN</button>
+        <button class="btn-large light-blue darken-4 book_btn">BOOK A VISIT!</button>
+    </form>
+</div>
+
+@endsection
